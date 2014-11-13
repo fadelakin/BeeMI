b05d62fadb92b9c7c335e98b8bc5b4a11e05f41a
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-11-13 13:18:48 -0500

    begin functionality

--- app/src/main/java/com/fisheradelakin/beemi/MainActivity.java
+++ app/src/main/java/com/fisheradelakin/beemi/MainActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +15,41 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void calculate(View v) {
+        if(v.getId() == R.id.calculateButton) {
+            EditText weightText = (EditText) findViewById(R.id.weightText);
+            EditText heightText = (EditText) findViewById(R.id.heightText);
+            TextView resultText = (TextView) findViewById(R.id.resultLabel);
+
+            float weight = Float.parseFloat(weightText.getText().toString());
+            float height = Float.parseFloat(heightText.getText().toString());
+
+            float bmiValue = calculateBMI(weight, height);
+
+            String bmiInterpretation = interpretBMI(bmiValue);
+
+            resultText.setText(bmiValue + "-" + bmiInterpretation);
+        }
+    }
+
+    private float calculateBMI(float weight, float height) {
+        return (float) (weight * 4.88 / (height * height));
+    }
+
+    private String interpretBMI(float bmiValue) {
+        if(bmiValue < 16) {
+            return "Severely underweight";
+        } else if(bmiValue < 18.5) {
+            return "Underweight";
+        } else if(bmiValue < 25) {
+            return "Normal";
+        } else if(bmiValue < 30) {
+            return "Overweight";
+        } else {
+            return "Obese";
+        }
     }
 
 
