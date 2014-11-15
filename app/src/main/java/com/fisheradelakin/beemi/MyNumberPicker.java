e6c68dca1f2bccaa5a71f6fb597287abfbc9ac26
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-11-15 18:40:27 -0500

    Add numberpicker

---
+++ app/src/main/java/com/fisheradelakin/beemi/MyNumberPicker.java
@@ -0,0 +1,31 @@
+package com.fisheradelakin.beemi;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.NumberPicker;
+
+/**
+ * Created by Fisher on 11/15/14.
+ */
+public class MyNumberPicker extends NumberPicker {
+
+    public MyNumberPicker(Context context) {
+        super(context);
+    }
+
+    public MyNumberPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        processAttributeSet(attrs);
+    }
+
+    public MyNumberPicker(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        processAttributeSet(attrs);
+    }
+
+    private void processAttributeSet(AttributeSet attrs) {
+        //This method reads the parameters given in the xml file and sets the properties according to it
+        this.setMinValue(attrs.getAttributeIntValue(null, "min", 0));
+        this.setMaxValue(attrs.getAttributeIntValue(null, "max", 0));
+    }
+}
